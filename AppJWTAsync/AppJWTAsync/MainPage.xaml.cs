1ebbc019a168abc47f9d2981e0f0b327b89fe966
Renan Beux <nanerxueb@hotmail.com>
2018-09-25 17:08:57 -0300

    250918 1708

--- AppJWTAsync/AppJWTAsync/MainPage.xaml.cs
+++ AppJWTAsync/AppJWTAsync/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using AppJWTAsync.Service;
 
 namespace AppJWTAsync
 {
@@ -13,5 +14,18 @@ namespace AppJWTAsync
 		{
 			InitializeComponent();
 		}
-	}
+
+        public async void GetTokenAction(object sender, EventArgs args)
+        {
+            string resultado = await JWTService.GetToken(nome.Text, password.Text);
+            LblToken.Text = resultado;
+        }
+
+        public async void VerificarAction(object sender, EventArgs args)
+        {
+            string resultado = await JWTService.Verificar();
+            LblResultado.Text = resultado;
+        }
+
+    }
 }
---
+++ AppJWTAsync/AppJWTAsync/Model/RespostaVerificar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppJWTAsync.Model
+{
+    public class RespostaVerificar
+    {
+        public string JWT { get; set; }
+        public Usuario usuario { get; set; }
+    }
+}
